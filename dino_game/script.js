e6cde4df19a72da510bea45a4720f359af018281
Aswanth611 <manjusachu2019@gmail.com>
2026-01-20 22:49:55 +0530

    Add files via upload

---
+++ dino_game/script.js
@@ -0,0 +1,38 @@
+var dino = document.getElementById("dino");
+var cactus = document.getElementById("cactus");
+var msg = document.getElementById("msg");
+
+dino.style.bottom = "0px";
+cactus.style.left = "600px";
+
+var gameOver = false;
+
+document.onkeydown = function () {
+    if (gameOver) return;
+
+    dino.style.bottom = "100px";
+
+    setTimeout(function () {
+        dino.style.bottom = "0px";
+    }, 300);
+};
+
+setInterval(function () {
+
+    if (gameOver) return;
+
+    var cactusLeft = parseInt(cactus.style.left);
+    cactus.style.left = (cactusLeft - 5) + "px";
+
+    if (cactusLeft < -30) {
+        cactus.style.left = "600px";
+    }
+
+    var dinoBottom = parseInt(dino.style.bottom);
+
+    if (cactusLeft < 90 && cactusLeft > 50 && dinoBottom == 0) {
+        msg.innerHTML = "GAME OVER";
+        gameOver = true;
+    }
+
+}, 20);
